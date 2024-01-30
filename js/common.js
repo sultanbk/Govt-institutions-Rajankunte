469b76bc3d2541a5799743fd4d8009097cbc3331
sultanbk <sbk74258@gmail.com>
2024-01-30 23:50:05 +0530

    design changes

--- js/common.js
+++ js/common.js
@@ -6,6 +6,12 @@ function setImageSource(elementId, imagePath) {
   }
 }
 
+// Home Page Slide Show 
+setImageSource('first-slide', 'images/slide show/slide1.jpg')
+setImageSource('second-slide', 'images/slide show/slide2.jpg')
+setImageSource('third-slide', 'images/slide show/slide3.jpg')
+setImageSource('fourth-slide', 'images/slide show/slide4.jpg')
+
 // Home Page images
 setImageSource('aboutUsImage', 'images/slide2.jpg');
 
